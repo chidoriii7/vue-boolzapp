1bcd608f351b80e8db7ea7786f480e33c7bf57cd
chidoriii7 <muratore.danilo@yahoo.it>
2024-05-24 16:50:02 +0200

    vue-boolzapp : milestone 2

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-
+// Milestone 2
+// Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// Click sul contatto mostra la conversazione del contatto cliccato
 
 
 
@@ -14,19 +16,20 @@ createApp({
                     name: 'Michele',
                     avatar: '/avatar_1.jpg',
                     visible: true,
-                    messages: [
+                    messages: 
+                    [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020 15:30',
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020 15:50',
                             message: 'Ricordati di stendere i panni',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 16:15:22',
+                            date: '10/01/2020 16:15',
                             message: 'Tutto fatto!',
                             status: 'received'
                         }
@@ -36,19 +39,20 @@ createApp({
                     name: 'Fabio',
                     avatar: '/avatar_2.jpg',
                     visible: true,
-                    messages: [
+                    messages: 
+                    [
                         {
-                            date: '20/03/2020 16:30:00',
+                            date: '20/03/2020 16:30',
                             message: 'Ciao come stai?',
                             status: 'sent'
                         },
                         {
-                            date: '20/03/2020 16:30:55',
+                            date: '20/03/2020 16:30',
                             message: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                         },
                         {
-                            date: '20/03/2020 16:35:00',
+                            date: '20/03/2020 16:35',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                             status: 'sent'
                         }
@@ -58,19 +62,20 @@ createApp({
                     name: 'Samuele',
                     avatar: '/avatar_3.jpg',
                     visible: true,
-                    messages: [
+                    messages: 
+                    [
                         {
-                            date: '28/03/2020 10:10:40',
+                            date: '28/03/2020 10:10',
                             message: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
-                            date: '28/03/2020 10:20:10',
+                            date: '28/03/2020 10:20',
                             message: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent'
                         },
                         {
-                            date: '28/03/2020 16:15:22',
+                            date: '28/03/2020 16:15',
                             message: 'Ah scusa!',
                             status: 'received'
                         }
@@ -80,14 +85,15 @@ createApp({
                     name: 'Alessandro B.',
                     avatar: '/avatar_4.jpg',
                     visible: true,
-                    messages: [
+                    messages: 
+                    [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020 15:30',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020 15:50',
                             message: 'Si, ma preferirei andare al cinema',
                             status: 'received'
                         }
@@ -97,14 +103,15 @@ createApp({
                     name: 'Alessandro L.',
                     avatar: '/avatar_5.jpg',
                     visible: true,
-                    messages: [
+                    messages: 
+                    [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020 15:30',
                             message: 'Ricordati di chiamare la nonna',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020 15:50',
                             message: 'Va bene, stasera la sento',
                             status: 'received'
                         }
@@ -114,19 +121,20 @@ createApp({
                     name: 'Claudia',
                     avatar: '/avatar_6.jpg',
                     visible: true,
-                    messages: [
+                    messages: 
+                    [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020 15:30',
                             message: 'Ciao Claudia, hai novità?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020 15:50',
                             message: 'Non ancora',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '10/01/2020 15:51',
                             message: 'Nessuna nuova, buona nuova',
                             status: 'sent'
                         }
@@ -136,14 +144,15 @@ createApp({
                     name: 'Federico',
                     avatar: '/avatar_7.jpg',
                     visible: true,
-                    messages: [
+                    messages: 
+                    [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020 15:30',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020 15:50',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
                             status: 'received'
                         }
@@ -153,30 +162,36 @@ createApp({
                     name: 'Davide',
                     avatar: '/avatar_8.jpg',
                     visible: true,
-                    messages: [
+                    messages: 
+                    [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020 15:30',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020 15:50',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '10/01/2020 15:51',
                             message: 'OK!!',
                             status: 'received'
                         }
                     ],
                 }
-            ]
+            ],
             
+            selectionContact: 0
         }
     },
     
     methods: {
-
+        changeContact(index) {
+            // console.log('ho cliccato un contatto');
+            // console.log(index);
+            this.selectionContact = index;
+        }
     }
 }).mount('#app')
